1953185731197f4d917dfa33546d1bd0d14f1fe3
Sergei Gordeev <gordeser@cvut.cz>
2023-03-02 14:55:43 +0100

    Add socket server

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-# init
+import socket
+
+SERVER = '192.168.56.1'
+PORT = 12345
+
+
+def start_server(server):
+    print("[SERVER] Starting server")
+
+    # Checking whether port is free
+    try:
+        server.bind((SERVER, PORT))
+    except ConnectionError as e:
+        print(e)
+
+    server.listen()
+    print(f"[SERVER] Listening on {SERVER}:{PORT}")
+
+    while True:
+        connection, address = server.accept()
+        print(f"[SERVER] New active connection {connection} {address}")
+
+
+def stop_server(server):
+    print("[SERVER] Stopping server")
+    server.close()
+
+
+def main():
+    server = socket.socket()
+    start_server(server)
+    stop_server(server)
+
+
+if __name__ == '__main__':
+    main()

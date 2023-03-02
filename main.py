b6b61b24513233b23dcdf363c4aa11e367be925d
Sergei Gordeev <gordeser@cvut.cz>
2023-03-02 15:10:00 +0100

    Add enum of server messages

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from enum import Enum
 import socket
 
 SERVER = '192.168.56.1'
 PORT = 12345
+
+
+class Messages(Enum):
+    SERVER_MOVE = '102 MOVE\a\b'
+    SERVER_TURN_LEFT = '103 TURN LEFT\a\b'
+    SERVER_TURN_RIGHT = '104 TURN RIGHT\a\b'
+    SERVER_PICK_UP = '105 GET MESSAGE\a\b'
+    SERVER_LOGOUT = '106 LOGOUT\a\b'
+    SERVER_KEY_REQUEST = '107 KEY REQUEST\a\b'
+    SERVER_OK = '200 OK\a\b'
+    SERVER_LOGIN_FAILED = '300 LOGIN FAILED\a\b'
+    SERVER_SYNTAX_ERROR = '301 SYNTAX ERROR\a\b'
+    SERVER_LOGIC_ERROR = '302 LOGIC ERROR\a\b'
+    SERVER_KEY_OUT_OF_RANGE_ERROR = '303 KEY OUT OF RANGE\a\b'
 
 
 def start_server(server):

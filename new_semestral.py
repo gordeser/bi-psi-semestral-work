d6bb5e21d76ee4b3f20ab149bd9e4319f1e3734e
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-04-04 00:03:19 +0200

    [NEW] Use one time ending in send_data instead of adding it to everything

--- new_semestral.py
+++ new_semestral.py
@@ -18,17 +18,17 @@ CLIENT_KEYS = [32037, 29295, 13603, 29533, 21952]
 
 
 class Messages(Enum):
-    SERVER_MOVE = "102 MOVE\a\b"
-    SERVER_TURN_LEFT = "103 TURN LEFT\a\b"
-    SERVER_TURN_RIGHT = "104 TURN RIGHT\a\b"
-    SERVER_PICK_UP = "105 GET MESSAGE\a\b"
-    SERVER_LOGOUT = "106 LOGOUT\a\b"
-    SERVER_KEY_REQUEST = "107 KEY REQUEST\a\b"
-    SERVER_OK = "200 OK\a\b"
-    SERVER_LOGIN_FAILED = "300 LOGIN FAILED\a\b"
-    SERVER_SYNTAX_ERROR = "301 SYNTAX ERROR\a\b"
-    SERVER_LOGIC_ERROR = "302 LOGIC ERROR\a\b"
-    SERVER_KEY_OUT_OF_RANGE_ERROR = "303 KEY OUT OF RANGE\a\b"
+    SERVER_MOVE = "102 MOVE"
+    SERVER_TURN_LEFT = "103 TURN LEFT"
+    SERVER_TURN_RIGHT = "104 TURN RIGHT"
+    SERVER_PICK_UP = "105 GET MESSAGE"
+    SERVER_LOGOUT = "106 LOGOUT"
+    SERVER_KEY_REQUEST = "107 KEY REQUEST"
+    SERVER_OK = "200 OK"
+    SERVER_LOGIN_FAILED = "300 LOGIN FAILED"
+    SERVER_SYNTAX_ERROR = "301 SYNTAX ERROR"
+    SERVER_LOGIC_ERROR = "302 LOGIC ERROR"
+    SERVER_KEY_OUT_OF_RANGE_ERROR = "303 KEY OUT OF RANGE"
 
 
 def get_data(connection, data):
@@ -42,7 +42,7 @@ def get_data(connection, data):
 
 
 def send_data(connection, data):
-    connection.send(bytes(data, 'utf-8'))
+    connection.send(bytes(str(data)+ENDING, 'utf-8'))
 
 
 def count_hash(username):
